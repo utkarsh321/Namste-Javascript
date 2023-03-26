d6ffc865d32e101990c333a60adb60dcfcc501e6
utkarshpgandhi12 <utkarshpgandhi1990@gmail.com>
2023-03-26 18:09:17 -0400

    First React App

---
+++ app.js
@@ -0,0 +1,18 @@
+const heading = React.createElement(
+    "h1",{},"Hello World from React!");
+        console.log(heading);
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+        root.render(heading);
+        
+
+const parent = React.createElement("div",{id : "parent"},[
+React.createElement{"div",},[
+    React.createElement("h1",{},"I'm on h1 tag"),
+    React.createElement("h2",{},"I'm on the h2 tag"),
+]),
+React.createElement("div",{id: child2"},[
+    React.createElement("h1",{},"I'm on the h1 tag"),
+    React.createElement("h2",{},"i'm on the h2 tag")
+]),
+]);
+
